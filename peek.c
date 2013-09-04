60d3c4f92a607b8cf96e89c8f219eb22beae04ba
s-p-k <mr.dwts@gmail.com>
2013-09-05 00:15:22 +0300

    check if  is set

--- peek.c
+++ peek.c
@@ -7,6 +7,7 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 
 void
@@ -22,10 +23,10 @@ usage(void)
 int
 main(int argc, char *argv[])
 {
-
+	char *envp;
 	char ver[] = "0.1.1";
 	int opt;
-
+	int eflag = 0;
 	if (argc == 1) {
 		fprintf(stderr, "Wrong usage: see %s -h\n", argv[0]);
 		return 1;
@@ -34,7 +35,7 @@ main(int argc, char *argv[])
 	if (argc == 2)
 		readonly(argv[1]);
 
-	while ((opt = getopt(argc, argv, "vh")) != -1){
+	while ((opt = getopt(argc, argv, "vhe:")) != -1){
 		switch(opt){
 		case 'v':
 			printf("The current version: %s\n", ver);
@@ -42,10 +43,19 @@ main(int argc, char *argv[])
 		case 'h':
 			usage();
 			break;
+		case 'e':
+			eflag = 1;
+			break;
 		default:
 			usage();
 			return 1;
 		}
+	}
+
+	if (eflag == 1){
+		envp = getenv("EDITOR");
+		if (!envp)
+			fprintf(stderr, "You must set your $EDITOR variable\n");
 	}
 
 	return 0;
