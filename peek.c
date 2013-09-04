3d8ed3484963414aa35e52e2f3dabd0e8bb03f31
s-p-k <mr.dwts@gmail.com>
2013-09-04 22:17:10 +0300

    added -r option to open a file readonly

--- peek.c
+++ peek.c
@@ -9,48 +9,40 @@
 #include <stdio.h>
 #include <unistd.h>
 
-#define DEF_DIR "/home/spk/repos/c/peek/csdir"
-
 void
 usage(void)
 {
-	fprintf(stderr, "Usage: peek [-v][-f file][-h]\n");
+	fprintf(stderr, "Usage: peek [-v][-r file][-h]\n");
 	return;
 }
 
 int
 main(int argc, char *argv[])
 {
-	
-	struct stat buffer;
-	char ver[] = "0.1.1";
-	int opt, status;
-	int hflag = 0, eflag = 0;
 
-	/* the cheatsheet to be read, edited or created */
-	const char *filename = "unspecified"; 
+	char ver[] = "0.1.1";
+	char *filename;
+	char buf[1000];
+	int opt, fd;
+	int rflag = 0;
+	FILE *fp;
 	
-	if (argc == 1)
-	{
+	if (argc == 1) {
 		fprintf(stderr, "Wrong usage: see %s -h\n", argv[0]);
 		return 1;
 	}
 	
-	while ((opt = getopt(argc, argv, "vhf:e:")) != -1)
-	{
-		switch(opt)
-		{
+	while ((opt = getopt(argc, argv, "vhr:")) != -1){
+		switch(opt){
 		case 'v':
 			printf("The current version: %s\n", ver);
 			break;
 		case 'h':
-			hflag = 1;
 			usage();
 			break;
-		case 'e':
+		case 'r':
 			filename = optarg;
-			eflag = 1;
-			printf("You have chosen to edit file %s\n", filename);
+			rflag = 1;
 			break;
 		default:
 			usage();
@@ -59,21 +51,21 @@ main(int argc, char *argv[])
 
 	}
 
-	status = stat(filename, &buffer);
-	
-	if (argc == 2)
-	{
-		filename = argv[1];
-		status = stat(filename, &buffer);
-		if (status == 0)
-			printf("Here we should open the file in argv[1]");
+	if (rflag == 1) {
+		fd = open(filename, O_RDONLY);
+		if (fd < 0) {
+			fprintf(stderr, "file %s does not exist\n", filename);
+			return 1;
+		} else {
+			fp = fopen(filename, "r");
+			if (!fp)
+				return 1;
+		}
+		while (fgets(buf,1000, fp) != NULL)
+			printf("%s", buf);
+		
+		fclose(fp);
 	}
-	
-	if (hflag == 0 && (status = stat(filename, &buffer) != 0))
-		fprintf(stderr, "file %s does not exist\n", filename);
-
-	if (eflag == 1)
-		printf("Open file indicated from -e option\n");
 
 	return 0;
 }
