7690596bac6654efe13945e1c25633bfab5685b8
s-p-k <mr.dwts@gmail.com>
2013-09-06 23:48:39 +0300

    added file handling functions

--- peek.c
+++ peek.c
@@ -5,29 +5,38 @@
 
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <sys/types.h>
+
 #include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <err.h>
 #include <string.h>
+#include <dirent.h>
 
 #include "peek.h"
 
 int
 main(int argc, char *argv[])
 {
+	
 	int opt;
 	int eflag = 0, hflag = 0;
+	char *cheatsheet;
 
 	if (argc == 1)
 		errx(1, "see %s -h", argv[0]);
 	
-	while ((opt = getopt(argc, argv, "he:l")) != -1) {
+	while ((opt = getopt(argc, argv, "hc:e:l")) != -1) {
 		switch(opt){
 		case 'h':
 			hflag = 1;
 			usage();
+			break;
+		case 'c':
+			cheatsheet = optarg;
+			createcheatsheet(cheatsheet);
 			break;
 		case 'e':
 			eflag = 1;
--- peek.h
+++ peek.h
@@ -3,10 +3,13 @@
 
 #define CHEATSHEET_DIR "/home/spk/.peek/"
 
+#define TMP_DIR "/tmp/"
+
 void
 usage(void)
 {
 	printf("Usage: peek [-h][-l][-e file][file]\n");
+	printf("see also the peek man page\n");
 	return;
 }
 
@@ -33,7 +36,19 @@ openfile(char *f, char *mode)
 	return 0;
 }
 
+/* Create a new cheatsheet: used for the -c option */
+void
+createcheatsheet(char *f)
+{
+	char newfile[50] = CHEATSHEET_DIR;
+	strcat(newfile, f);
+	printf("You want to create file %s\n", newfile);
+	
+	return;
+}
+	
 /* list cheatsheets in CHEATSHEET_DIR */
+/* must make the printing prettier */
 
 void
 listcheatsheets(char *dr)
@@ -46,13 +61,40 @@ listcheatsheets(char *dr)
 	if (!d)
 		err(1, "yo mate! %s", dr);
 
-	while ((dir = readdir(d)) != NULL) 
+	while ((dir = readdir(d)) != NULL)
 		printf("%s ", dir->d_name);
+
 	printf("\n");
 
 	closedir(d);
 
 	return;
+}
+
+/* creates a copy of a cheatsheet, before opening it for edit with -e option */
+
+void
+makecopy(char *f)
+{
+	int ch;
+	FILE *fp1, *fp2;
+
+	fp1 = fopen(f, "r"); /* original file */
+
+	/* need to make the fp2 file look like: /tmp/cheatsheet_name.peek */
+	fp2 = fopen("TMP_DIR/f/backup/", "w"); /* backup file in /tmp */
+
+	while (1) {
+		ch = fgetc(fp1);
+
+		if (ch == EOF)
+			break;
+		else
+			putc(ch, fp2);
+	}
+
+	fclose(fp1);
+	fclose(fp2);
 }
 
 #endif
