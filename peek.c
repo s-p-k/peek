563acb614a81a8cd2728ab1440a416a88d08366b
s-p-k <mr.dwts@gmail.com>
2013-09-07 17:02:23 +0300

    editsheet does not need two arguments

--- peek.c
+++ peek.c
@@ -15,7 +15,24 @@
 #include <string.h>
 #include <dirent.h>
 
-#include "peek.h"
+#define CHEATSHEET_DIR "/home/spk/.peek/"
+#define EDITOR "/usr/bin/mg"
+#define BIN_NAME "mg"
+
+void
+usage(void)
+{
+	printf("Usage: peek [-h][-l][-e file][file]\n");
+	printf("see also the peek man page\n");
+}
+
+void readsheet(char *f);
+
+void editsheet(char *f);
+
+void createsheet(char *f);
+
+void listsheets(char *dr);
 
 int
 main(int argc, char *argv[])
@@ -45,7 +62,7 @@ main(int argc, char *argv[])
 	}
 
 	if (argc == 2 && hflag == 0)
-		readsheet(argv[1], "r");
+		readsheet(argv[1]);
 
 /* open existing file for edit */
 
@@ -53,4 +70,96 @@ main(int argc, char *argv[])
 		editsheet(argv[2]);
 
 	return 0;
+}
+
+void
+readsheet(char *f)
+{
+	int c;
+	FILE *fpoint;
+	char readfile[100] = CHEATSHEET_DIR;
+
+	strcat(readfile, f);
+
+	fpoint = fopen(readfile, "r");
+
+	if (!fpoint)
+		err(1, "%s", f);
+
+	while((c = fgetc(fpoint)) != EOF)
+		printf("%c", c);
+	
+	fclose(fpoint);
+
+	return;
+}
+	
+void
+editsheet(char *f)
+{
+
+	int ret;
+	FILE *fpoint;
+	char file[100] = CHEATSHEET_DIR;
+
+	strcat(file, f);
+
+	fpoint = fopen(file, "a+");
+
+	if (!fpoint)
+		err(1, "editsheet %s", f);
+
+	ret = execl(EDITOR, BIN_NAME, file, (char *)0);
+
+	if (ret == -1)
+		err(1, "editsheet: check in peek.h if all variables are set\n");
+
+	fclose(fpoint);
+
+	return;
+}
+
+/* Create a new cheatsheet */
+
+void
+createsheet(char *f)
+{
+	FILE *fpoint;
+	char newfile[50] = CHEATSHEET_DIR;
+
+	strcat(newfile, f);
+
+	fpoint = fopen(newfile, "wx");
+
+	if (!fpoint)
+		err(1, "YOOOOO %s", newfile);
+
+	fclose(fpoint);
+
+	return;
+}
+
+/* list cheatsheets in CHEATSHEET_DIR: must make the printing prettier */
+
+void
+listsheets(char *dr)
+{
+	DIR *d;
+	struct dirent *dir;
+
+	d = opendir(dr);
+
+	if (!d)
+		err(1, "listsheets %s", dr);
+
+	printf("Available cheatsheets:\n");
+
+	while ((dir = readdir(d)) != NULL)
+		printf("%s ", dir->d_name);
+
+	printf("\n");
+
+	closedir(d);
+
+	return;
 }
